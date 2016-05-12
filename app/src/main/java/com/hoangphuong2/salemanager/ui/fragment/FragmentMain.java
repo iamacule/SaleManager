5861d024b5f29157e4b2cd77c18829b4e74d74ba
AnPham <an.phamhung@gmail.com>
2016-05-12 17:02:00 +0700

    update main view

--- app/src/main/java/com/hoangphuong2/salemanager/ui/fragment/FragmentMain.java
+++ app/src/main/java/com/hoangphuong2/salemanager/ui/fragment/FragmentMain.java
@@ -1,21 +1,15 @@
 package com.hoangphuong2.salemanager.ui.fragment;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 
 import com.hoangphuong2.salemanager.R;
 import com.hoangphuong2.salemanager.ui.control.OnSingleClickListener;
-import com.hoangphuong2.salemanager.util.ResizeBitmap;
-import com.hoangphuong2.salemanager.util.ScreenUtil;
 
 /**
  * Created by MrAn on 12-May-16.
@@ -24,12 +18,10 @@ public class FragmentMain extends Fragment {
     private ImageButton btnPrivate;
     private ImageButton btnCompany;
     private ImageButton btnBill;
-    private float screenWidth;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
-        screenWidth = ScreenUtil.getScreenWidth(getActivity().getWindowManager());
         initValue(view);
         return view;
     }
